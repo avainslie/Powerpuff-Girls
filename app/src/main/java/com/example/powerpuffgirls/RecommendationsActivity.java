602afe66b3a1fe009ecd01b54cb9cbbe9ee55ee0
avainslie <50717419+avainslie@users.noreply.github.com>
2021-01-06 19:43:43 -0700

    Items are clickable

--- app/src/main/java/com/example/powerpuffgirls/RecommendationsActivity.java
+++ app/src/main/java/com/example/powerpuffgirls/RecommendationsActivity.java
@@ -21,12 +21,14 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-public class RecommendationsActivity extends AppCompatActivity {
+// TODO: Add intent when click on resource and filter by user info on clicking filter btn
+
+public class RecommendationsActivity extends AppCompatActivity implements FirestoreAdapter.OnListItemClick {
 
     private static final String TAG = "hello" ;
     private RecyclerView ResourceRecycler;
     private FirebaseFirestore firebaseFirestore;
-    private FirestoreRecyclerAdapter adapter;
+    private FirestoreAdapter adapter;
 
 
     // https://www.youtube.com/watch?v=cBwaJYocb9I&ab_channel=TVACStudio
@@ -48,20 +50,7 @@ public class RecommendationsActivity extends AppCompatActivity {
 
 
         // Adapter
-        adapter = new FirestoreRecyclerAdapter<Resources, ResourcesViewHolder>(options) {
-            @NonNull
-            @Override
-            public ResourcesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_single, parent, false);
-                return new ResourcesViewHolder(view);
-            }
-
-            @Override
-            protected void onBindViewHolder(@NonNull ResourcesViewHolder holder, int position, @NonNull Resources model) {
-                holder.ResourceName.setText(model.getName());
-                holder.ResourceDescription.setText(model.getDescription());
-            }
-        };
+        adapter = new FirestoreAdapter(options, this);
 
         ResourceRecycler.setHasFixedSize(true);
         ResourceRecycler.setLayoutManager(new LinearLayoutManager( this));
@@ -69,18 +58,6 @@ public class RecommendationsActivity extends AppCompatActivity {
 
     }
 
-    private class ResourcesViewHolder extends RecyclerView.ViewHolder{
-
-        private TextView ResourceName;
-        private TextView ResourceDescription;
-
-        public ResourcesViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            ResourceName = itemView.findViewById(R.id.ResourceName);
-            ResourceDescription = itemView.findViewById(R.id.ResourceDescription);
-        }
-    }
 
     @Override
     protected void onStop() {
@@ -93,5 +70,18 @@ public class RecommendationsActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         adapter.startListening();
+    }
+
+    User user = new User("Ainslie","Biology");
+
+    public void filter(View v){
+        if (user.getDegreeMajor().equals("Biology")){ // replace "biology" with get text from resource
+
+        }
+    }
+
+    @Override
+    public void onItemClick() {
+        Log.d("ITEM CLICK", "THE ITEM IS CLICKED");
     }
 }
