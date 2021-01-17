a4f7e108824c14ad03f1da5deab3596dfadc99bf
candynwigwe <47461673+candynwigwe@users.noreply.github.com>
2021-01-16 20:30:19 -0700

    Add files via upload

---
+++ app/src/main/java/com/example/powerpuffgirlsuser/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.powerpuffgirlsuser;
+import com.example.powerpuffgirlsuser.ui.login.LoginActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button signUp= findViewById(R.id.signUpButton);
+        signUp.setOnClickListener(signUpView -> openSignUpActivity());
+
+        Button logIn = findViewById(R.id.logInButton);
+        logIn.setOnClickListener(logInView -> openLogInActivity());
+    }
+
+    public void openLogInActivity() {
+        Intent logInIntent = new Intent(this, LoginActivity.class);
+        startActivity(logInIntent);
+    }
+    public void openSignUpActivity(){
+        Intent signUpIntent = new Intent(this, SignUpActivity.class );
+        startActivity(signUpIntent);
+    }
+
+
+
+}
